40894256ac664c634908d1ebeb73e78ee12dfdf7
crispy78 <42983001+crispy78@users.noreply.github.com>
2023-04-14 09:32:58 +0200

    Update findmy.sh
    Removed the infinite loop; it now runs only once so it can be triggered from within an automation.

--- findmy.sh
+++ findmy.sh
@@ -5,75 +5,74 @@
 
 # This project is far from done and it still needs some work:
 # - Instead of a device_tracker I would like to create a Device with a Device_tracker entity,
-# - In my setup it has difficulty with the logbook entries and history.
 
 # Setting the variables
 # location of the script
 scriptlocation=/config/custom_components/find_my
 
 # IP address of the jailbroken iOS device, accessible from HA through SSH
-iosdevice=192.168.1.101
+iosdevice=192.168.1.110
 
 # IP address of the MQTT broker
-mqttbroker=192.168.1.10
+mqttbroker=192.168.1.100
 
 # MQTT broker username
 mqttusername=username
 
 # MQTT broker password
-mqttpassword=password
+mqttpassword=username
 
 
 #Start an infinite loop
-while :
+echo $(date -u) "- Download Items.data from iOS device ($iosdevice)"
+scp root@$iosdevice:/User/Library/Caches/com.apple.findmy.fmipcore/Items.data $scriptlocation/Items.data > /dev/null 2>&1
+
+object=`cat $scriptlocation/Items.data | jq ".[].serialNumber" | wc -l`
+echo $(date -u) "- Number of Apple Find My objects to process: $object"
+object=`echo "$(($object-1))"`
+echo "-----------------------------------------------------------------------------------------------------------------------------"
+for j in $(seq 0 $object)
+
 do
+echo $(date -u) "- Gathering data from $scriptlocation/Items.data for next Apple Find My object to process"
+datetime=`date +"%Y-%m-%d  %T"`
 
-	scp root@$iosdevice:/User/Library/Caches/com.apple.findmy.fmipcore/Items.data $scriptlocation/Items.data
+serialnumber=`cat $scriptlocation/Items.data | jq -r ".[$j].serialNumber"`
+manufacturerName=`cat $scriptlocation/Items.data | jq -r ".[$j].productType.productInformation.manufacturerName"`
+modelName=`cat $scriptlocation/Items.data | jq -r ".[$j].productType.productInformation.modelName"`
+systemversion=`cat $scriptlocation/Items.data | jq -r ".[$j].systemVersion"`
 
-  	airtagsnumber=`cat $scriptlocation/Items.data | jq ".[].serialNumber" | wc -l`
-	airtagsnumber=`echo "$(($airtagsnumber-1))"`
+antennapower=`cat $scriptlocation/Items.data | jq -r ".[$j].productType.productInformation.antennaPower"`
+batterystatus=`cat $scriptlocation/Items.data | jq -r ".[$j].batteryStatus"`
+name=`cat $scriptlocation/Items.data | jq -r ".[$j].name"`
 
-	for j in $(seq 0 $airtagsnumber)
-	do
-	datetime=`date +"%Y-%m-%d  %T"`
+locationtimestamp=`cat $scriptlocation/Items.data | jq -r ".[$j].location.timeStamp"`
+locationpositiontype=`cat $scriptlocation/Items.data | jq -r ".[$j].location.positionType"`
+locationlatitude=`cat $scriptlocation/Items.data | jq -r ".[$j].location.latitude"`
+locationlongitude=`cat $scriptlocation/Items.data | jq -r ".[$j].location.longitude"`
+locationverticalaccuracy=`cat $scriptlocation/Items.data | jq -r ".[$j].location.verticalAccuracy" | sed 's/null/0/g'`
+locationhorizontalaccuracy=`cat $scriptlocation/Items.data | jq -r ".[$j].location.horizontalAccuracy" | sed 's/null/0/g'`
+locationfloorlevel=`cat $scriptlocation/Items.data | jq -r ".[$j].location.floorlevel" | sed 's/null/0/g'`
+locationaltitude=`cat $scriptlocation/Items.data | jq -r ".[$j].location.altitude" | sed 's/null/0/g'`
+locationisinaccurate=`cat $scriptlocation/Items.data | jq -r ".[$j].location.isInaccurate" | awk '{ print "\""$0"\"" }'`
+locationisold=`cat $scriptlocation/Items.data | jq -r ".[$j].location.isOld" | awk '{ print "\""$0"\"" }' `
+locationfinished=`cat $scriptlocation/Items.data | jq -r ".[$j].location.locationFinished" | awk '{ print "\""$0"\"" }' `
 
-	serialnumber=`cat $scriptlocation/Items.data | jq -r ".[$j].serialNumber"`
-	manufacturerName=`cat $scriptlocation/Items.data | jq -r ".[$j].productType.productInformation.manufacturerName"`
-	modelName=`cat $scriptlocation/Items.data | jq -r ".[$j].productType.productInformation.modelName"`
-	systemversion=`cat $scriptlocation/Items.data | jq -r ".[$j].systemVersion"`
+addressmapItemFullAddress=`cat $scriptlocation/Items.data | jq -r ".[$j].address.mapItemFullAddress" | sed 's/null/""/g'`
+addressstreetName=`cat $scriptlocation/Items.data | jq -r ".[$j].address.streetName"| sed 's/null/""/g'`
+addressstreetaddress=`cat $scriptlocation/Items.data | jq -r ".[$j].address.streetAddress"| sed 's/null/""/g'`
+addresslocality=`cat $scriptlocation/Items.data | jq -r ".[$j].address.locality"| sed 's/null/""/g'`
+addressadministrativearea=`cat $scriptlocation/Items.data | jq -r ".[$j].address.administrativeArea"| sed 's/null/""/g'`
+addressstatecode=`cat $scriptlocation/Items.data | jq -r ".[$j].address.stateCode" | sed 's/null/""/g'`
+addresscountry=`cat $scriptlocation/Items.data | jq -r ".[$j].address.country"| sed 's/null/""/g'`
+addresscountrycode=`cat $scriptlocation/Items.data | jq -r ".[$j].address.countryCode"| sed 's/null/""/g'`
+addressareaofinteresta=`cat $scriptlocation/Items.data | jq -r ".[$j].address.areaOfInterest[0]" | sed 's/null/""/g'`
+addressareaofinterestb=`cat $scriptlocation/Items.data | jq -r ".[$j].address.areaOfInterest[1]" | sed 's/null/""/g'`
+echo $(date -u) "- Data gathered, going to send data to MQTT broker $mqttbroker"
 
-	antennapower=`cat $scriptlocation/Items.data | jq -r ".[$j].productType.productInformation.antennaPower"`
-	batterystatus=`cat $scriptlocation/Items.data | jq -r ".[$j].batteryStatus"`
-  	name=`cat $scriptlocation/Items.data | jq -r ".[$j].name"`
-
-	locationtimestamp=`cat $scriptlocation/Items.data | jq -r ".[$j].location.timeStamp"`
-	locationpositiontype=`cat $scriptlocation/Items.data | jq -r ".[$j].location.positionType"`
-	locationlatitude=`cat $scriptlocation/Items.data | jq -r ".[$j].location.latitude"`
-	locationlongitude=`cat $scriptlocation/Items.data | jq -r ".[$j].location.longitude"`
-	locationverticalaccuracy=`cat $scriptlocation/Items.data | jq -r ".[$j].location.verticalAccuracy" | sed 's/null/0/g'`
-	locationhorizontalaccuracy=`cat $scriptlocation/Items.data | jq -r ".[$j].location.horizontalAccuracy" | sed 's/null/0/g'`
-	locationfloorlevel=`cat $scriptlocation/Items.data | jq -r ".[$j].location.floorlevel" | sed 's/null/0/g'`
-	locationaltitude=`cat $scriptlocation/Items.data | jq -r ".[$j].location.altitude" | sed 's/null/0/g'`
-	locationisinaccurate=`cat $scriptlocation/Items.data | jq -r ".[$j].location.isInaccurate" | awk '{ print "\""$0"\"" }'`
-	locationisold=`cat $scriptlocation/Items.data | jq -r ".[$j].location.isOld" | awk '{ print "\""$0"\"" }' `
-	locationfinished=`cat $scriptlocation/Items.data | jq -r ".[$j].location.locationFinished" | awk '{ print "\""$0"\"" }' `
-
-	addressmapItemFullAddress=`cat $scriptlocation/Items.data | jq -r ".[$j].address.mapItemFullAddress" | sed 's/null/""/g'`
- 	addressstreetName=`cat $scriptlocation/Items.data | jq -r ".[$j].address.streetName"| sed 's/null/""/g'`
-	addressstreetaddress=`cat $scriptlocation/Items.data | jq -r ".[$j].address.streetAddress"| sed 's/null/""/g'`
-	addresslocality=`cat $scriptlocation/Items.data | jq -r ".[$j].address.locality"| sed 's/null/""/g'`
-	addressadministrativearea=`cat $scriptlocation/Items.data | jq -r ".[$j].address.administrativeArea"| sed 's/null/""/g'`
-	addressstatecode=`cat $scriptlocation/Items.data | jq -r ".[$j].address.stateCode" | sed 's/null/""/g'`
-	addresscountry=`cat $scriptlocation/Items.data | jq -r ".[$j].address.country"| sed 's/null/""/g'`
-	addresscountrycode=`cat $scriptlocation/Items.data | jq -r ".[$j].address.countryCode"| sed 's/null/""/g'`
-	addressareaofinteresta=`cat $scriptlocation/Items.data | jq -r ".[$j].address.areaOfInterest[0]" | sed 's/null/""/g'`
-	addressareaofinterestb=`cat $scriptlocation/Items.data | jq -r ".[$j].address.areaOfInterest[1]" | sed 's/null/""/g'`
-
-  	mosquitto_pub -h $mqttbroker -u $mqttusername -P $mqttpassword -t homeassistant/device_tracker/findmy_$serialnumber/config -m '{"~": "homeassistant/device_tracker/findmy_'$serialnumber'", "stat_t": "~/state", "json_attr_t": "~/attributes", "name": "Apple FindMy '$name'"}'
-	mosquitto_pub -h $mqttbroker -u $mqttusername -P $mqttpassword -t homeassistant/device_tracker/findmy_$serialnumber/state -m ''"$addressmapItemFullAddress"''
-	mosquitto_pub -h $mqttbroker -u $mqttusername -P $mqttpassword -t homeassistant/device_tracker/findmy_$serialnumber/attributes -m '{"latitude": '$locationlatitude', "longitude": '$locationlongitude', "altitude": '$locationaltitude', "vertical accuracy": '$locationverticalaccuracy',"horizontal accuracy": '$locationhorizontalaccuracy', "battery_level": '$batterystatus', "antenna_power": '$antennapower'}'
-
-	done
-	sleep 60
-
+echo $(date -u) "- Sending MQTT data of Apple Find My object: $name"
+mosquitto_pub -h $mqttbroker -u $mqttusername -P $mqttpassword -t homeassistant/device_tracker/findmy_$serialnumber/config -m '{"~": "homeassistant/device_tracker/findmy_'$serialnumber'", "stat_t": "~/state", "json_attr_t": "~/attributes", "name": "Apple FindMy '$name'"}'
+mosquitto_pub -h $mqttbroker -u $mqttusername -P $mqttpassword -t homeassistant/device_tracker/findmy_$serialnumber/state -m ''"$addressmapItemFullAddress"''
+mosquitto_pub -h $mqttbroker -u $mqttusername -P $mqttpassword -t homeassistant/device_tracker/findmy_$serialnumber/attributes -m '{"latitude": '$locationlatitude', "longitude": '$locationlongitude', "altitude": '$locationaltitude', "vertical accuracy": '$locationverticalaccuracy',"horizontal accuracy": '$locationhorizontalaccuracy', "battery_level": '$batterystatus', "antenna_power": '$antennapower'}'
+echo -e $(date -u) "- Transfer of MQTT data completed \n"
 done
